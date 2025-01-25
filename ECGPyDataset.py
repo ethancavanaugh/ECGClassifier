f43a36a579bbc099eb0b78fa6c31f8f29e273bab
ethancavanaugh <ethancavanaugh@icloud.com>
2025-01-25 14:49:08 -0500

    Modifies ECGPyDataset to use preprocessed data

--- ECGPyDataset.py
+++ ECGPyDataset.py
@@ -1,15 +1,19 @@
-import keras
+import pandas as pd
 import numpy as np
+import keras
 import h5py
+import math
 
 
 class ECGPyDataset(keras.utils.PyDataset):
 
     #x_set: list of ECG reading file numbers
     #y_set: list of correct classes
-    def __init__(self, x_set, y_set, batch_size, **kwargs):
+    def __init__(self, df, batch_size, **kwargs):
         super().__init__(**kwargs)
-        self.x, self.y = x_set, y_set
+
+        self.x = df.ECG_ID
+        self.y = df.drop('ECG_ID', axis=1)
         self.batch_size = batch_size
 
     # Return number of batches
@@ -22,16 +26,16 @@ class ECGPyDataset(keras.utils.PyDataset):
         high = min(low + self.batch_size, len(self.x))
 
         batch_x = np.array([self._read_ecg(file) for file in self.x[low:high]])
-        print("low", low, "high", high)
+        batch_x = np.swapaxes(batch_x, 1, 2)
         batch_y = np.array(self.y[low:high])
-        print("x shape:", batch_x.shape)
-        print("y shape:", batch_y.shape)
+        #print("x shape:", batch_x.shape)
+        #print("y shape:", batch_y.shape)
 
-        return [batch_x, batch_y]
+        return batch_x, batch_y
 
     #Extract ECG tracings from their h5 file to a numpy array
-    def _read_ecg(self, n):
-        with h5py.File(f'data/records/A{n:05d}.h5', 'r') as f:
+    def _read_ecg(self, ecg_id):
+        with h5py.File(f'data/records_norm/{ecg_id:s}.h5', 'r') as f:
             signal = f['ecg'][()]
             #Take the first 10s of each reading
-            return signal[:,:5000]
+            return signal[:,:4096]
