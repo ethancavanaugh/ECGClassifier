af0b7ec38ad0f9f9a83e15a855abdbba1d2fb23c
ethancavanaugh <ethancavanaugh@icloud.com>
2025-01-25 14:51:09 -0500

    Creates basic evaluation script

---
+++ eval.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from tensorflow.keras.models import load_model
+from sklearn.metrics import classification_report
+
+from ECGPyDataset import ECGPyDataset
+
+if __name__ == "__main__":
+    model = load_model('logs/backup_model_last.keras')
+    test_df = pd.read_csv('./data/test.csv')
+    test_dataset = ECGPyDataset(test_df, 1)
+
+
+    yhat = model.predict(test_dataset)
+    print(yhat)
+    yhat = yhat.round()
+    ytrue = test_df.drop('ECG_ID', axis=1)
+
+
+    report = classification_report(ytrue, yhat)
+    print(report)
